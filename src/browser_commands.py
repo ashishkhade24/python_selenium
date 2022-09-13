73aa5063b928a59960c93d2e1fac16d61017d078
ashishkhade24 <ashishkhade11@gmail.com>
2022-09-13 14:39:00 +0530

    adding src folder

---
+++ src/browser_commands.py
@@ -0,0 +1,17 @@
+import time
+from selenium.webdriver.common.by import By
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://www.seleniumframework.com/demo-sites/")
+driver.save_screenshot("abc.jpg")
+print("Page Title = ", driver.title)
+print("Current URL = ", driver.current_url)
+print("Page Source = ", driver.page_source)
+
+driver.quit()
+
---
+++ src/dropdown_select.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# driver.get("http://demo.guru99.com/test/newtours/register.php")
+#
+# drpCountry = Select(driver.find_element(By.NAME,"country"))
+# time.sleep(2)
+# drpCountry.select_by_visible_text("ANTARCTICA")
+# time.sleep(2)
+# drpCountry.select_by_index(4)
+# time.sleep(2)
+# Selecting Items in a multiple select elements
+
+driver.get("http://jsbin.com/osebed/2")
+
+fruits = Select(driver.find_element(By.ID,"fruits"))
+
+assert fruits.is_multiple
+time.sleep(2)
+fruits.select_by_visible_text("Banana")
+time.sleep(2)
+fruits.deselect_by_visible_text("Banana")
+time.sleep(2)
+fruits.deselect_by_visible_text("Apple")
+time.sleep(2)
+fruits.select_by_index(1)
+time.sleep(2)
+driver.quit()
+
+
